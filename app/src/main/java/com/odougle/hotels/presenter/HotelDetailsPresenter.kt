bd6b7eb7a8a8d930ffa896e4790d8d2e7774d788
douguizilla <douglasgommez@gmail.com>
2021-08-17 12:27:30 -0300

    created class HotelDetailsPresenter.kt

---
+++ app/src/main/java/com/odougle/hotels/presenter/HotelDetailsPresenter.kt
@@ -0,0 +1,11 @@
+package com.odougle.hotels.presenter
+
+import com.odougle.hotels.model.HotelRepository
+import com.odougle.hotels.view.HotelDetailsView
+
+class HotelDetailsPresenter (
+    private val view: HotelDetailsView,
+    private val repository: HotelRepository
+) {
+
+}
