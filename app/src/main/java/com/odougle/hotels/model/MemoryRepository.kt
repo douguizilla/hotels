bb658d5800735878f09f705a3927fd6e5d0641ca
douguizilla <douglasgommez@gmail.com>
2021-08-17 12:01:04 -0300

    implemented remove, hotelById and search functions

--- app/src/main/java/com/odougle/hotels/model/MemoryRepository.kt
+++ app/src/main/java/com/odougle/hotels/model/MemoryRepository.kt
@@ -6,26 +6,26 @@ object MemoryRepository : HotelRepository {
     private val hotelList = mutableListOf<Hotel>()
 
     init {
-        save(Hotel(0,"New Beach Hotel","Av. Boa Viagem",4.5f))
-        save(Hotel(0,"Recife Hotel","Av. Boa Viagem",4.0f))
-        save(Hotel(0,"Canario Hotel","Av. dos Navegadores",3.0f))
-        save(Hotel(0,"Bianca Beach Hotel","Av. da Paixao",4.5f))
-        save(Hotel(0,"Caribe Hotel","Av. Carolina Lima",4.0f))
-        save(Hotel(0,"Olavio Hotel","Av. dos Caminhoneiros",3.0f))
-        save(Hotel(0,"New Orleans Hotel","Av. Pereira dos Santos",4.8f))
-        save(Hotel(0,"Cristina Hotel","Av. da Criança",4.1f))
-        save(Hotel(0,"Lima Hotel","Av. dos Pedreiros",3.7f))
+        save(Hotel(0, "New Beach Hotel", "Av. Boa Viagem", 4.5f))
+        save(Hotel(0, "Recife Hotel", "Av. Boa Viagem", 4.0f))
+        save(Hotel(0, "Canario Hotel", "Av. dos Navegadores", 3.0f))
+        save(Hotel(0, "Bianca Beach Hotel", "Av. da Paixao", 4.5f))
+        save(Hotel(0, "Caribe Hotel", "Av. Carolina Lima", 4.0f))
+        save(Hotel(0, "Olavio Hotel", "Av. dos Caminhoneiros", 3.0f))
+        save(Hotel(0, "New Orleans Hotel", "Av. Pereira dos Santos", 4.8f))
+        save(Hotel(0, "Cristina Hotel", "Av. da Criança", 4.1f))
+        save(Hotel(0, "Lima Hotel", "Av. dos Pedreiros", 3.7f))
     }
 
     override fun save(hotel: Hotel) {
-        if(hotel.id == 0L){
+        if (hotel.id == 0L) {
             hotel.id = nextId++
             hotelList.add(hotel)
-        }else{
+        } else {
             val index = hotelList.indexOfFirst {
                 it.id == hotel.id
             }
-            if(index > -1)
+            if (index > -1)
                 hotelList[index] = hotel
             else
                 hotelList.add(hotel)
@@ -33,14 +33,21 @@ object MemoryRepository : HotelRepository {
     }
 
     override fun remove(vararg hotels: Hotel) {
-        TODO("Not yet implemented")
+        hotelList.removeAll(hotels)
     }
 
     override fun hotelById(id: Long, callback: (Hotel?) -> Unit) {
-        TODO("Not yet implemented")
+        callback(hotelList.find { hotel ->
+            hotel.id == id
+        })
     }
 
     override fun search(term: String, callback: (List<Hotel?>) -> Unit) {
-        TODO("Not yet implemented")
+        callback(
+            if (term.isEmpty()) hotelList
+            else hotelList.filter { hotel ->
+                hotel.name.toUpperCase().contains(term.toUpperCase())
+            }
+        )
     }
 }
