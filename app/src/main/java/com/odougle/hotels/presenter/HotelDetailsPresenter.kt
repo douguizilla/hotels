7dba3d24c1c33aea40ef9d4d348eae63e0676570
douguizilla <douglasgommez@gmail.com>
2021-08-17 12:29:18 -0300

    implemented function to load hotel details

--- app/src/main/java/com/odougle/hotels/presenter/HotelDetailsPresenter.kt
+++ app/src/main/java/com/odougle/hotels/presenter/HotelDetailsPresenter.kt
@@ -7,5 +7,13 @@ class HotelDetailsPresenter (
     private val view: HotelDetailsView,
     private val repository: HotelRepository
 ) {
-
+    fun loadHotelDetails(id: Long){
+        repository.hotelById(id){hotel ->
+            if(hotel != null){
+                view.showHotelDetails(hotel)
+            }else{
+                view.errorHotelNotFound()
+            }
+        }
+    }
 }
