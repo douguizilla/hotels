2726270bb1999cb062f5b4a13637aa89d05e12d6
douguizilla <douglasgommez@gmail.com>
2021-08-18 13:48:04 -0300

    implemented necessary changes to a tablet screen

--- app/src/main/java/com/odougle/hotels/view/HotelActivity.kt
+++ app/src/main/java/com/odougle/hotels/view/HotelActivity.kt
@@ -2,6 +2,8 @@ package com.odougle.hotels.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.odougle.hotels.R
 import com.odougle.hotels.databinding.ActivityHotelBinding
 import com.odougle.hotels.model.Hotel
 
@@ -17,12 +19,25 @@ class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListene
     }
 
     override fun onHotelClick(hotel: Hotel) {
-        showDetailsActivity(hotel.id)
+        if(isTablet()){
+            showDetailsFragment(hotel.id)
+        }else{
+            showDetailsActivity(hotel.id)
+        }
     }
 
     private fun showDetailsActivity(hotelId: Long) {
         HotelDetailsActivity.open(this,hotelId)
     }
 
+    private fun isTablet() = findViewById<View>(R.id.details) != null
+
+    private fun showDetailsFragment(hotelId: Long){
+        val fragment = HotelDetailsFragment.newInstance(hotelId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.details, fragment, HotelDetailsFragment.TAG_DETAILS)
+            .commit()
+    }
 
 }
