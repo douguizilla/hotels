d44cffd18f4f3b57d4d3e8a3fcf233ff1b367f84
douguizilla <douglasgommez@gmail.com>
2021-08-18 13:07:13 -0300

    implemented HotelListFragment.OnHotelClickListener interface on HotelActivity.kt to call HotelDetailsActivity.open() passing context and hotel id as parameters

--- app/src/main/java/com/odougle/hotels/view/HotelActivity.kt
+++ app/src/main/java/com/odougle/hotels/view/HotelActivity.kt
@@ -3,8 +3,9 @@ package com.odougle.hotels.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odougle.hotels.databinding.ActivityHotelBinding
+import com.odougle.hotels.model.Hotel
 
-class HotelActivity : AppCompatActivity() {
+class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListener {
 
     private val binding: ActivityHotelBinding by lazy {
         ActivityHotelBinding.inflate(layoutInflater)
@@ -14,4 +15,14 @@ class HotelActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
+
+    override fun onHotelClick(hotel: Hotel) {
+        showDetailsActivity(hotel.id)
+    }
+
+    private fun showDetailsActivity(hotelId: Long) {
+        HotelDetailsActivity.open(this,hotelId)
+    }
+
+
 }
