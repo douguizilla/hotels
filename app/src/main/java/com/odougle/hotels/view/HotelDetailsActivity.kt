d940f54dfe83e827b0ede0803de893534fe488c1
douguizilla <douglasgommez@gmail.com>
2021-08-18 12:45:21 -0300

    implemented show hotel details fragment to replace with new Hotel details fragment instance by id

--- app/src/main/java/com/odougle/hotels/view/HotelDetailsActivity.kt
+++ app/src/main/java/com/odougle/hotels/view/HotelDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.odougle.hotels.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.hotels.R
 import com.odougle.hotels.databinding.ActivityHotelDetailsBinding
 
 class HotelDetailsActivity : AppCompatActivity() {
@@ -9,8 +10,24 @@ class HotelDetailsActivity : AppCompatActivity() {
     private val binding: ActivityHotelDetailsBinding by lazy{
         ActivityHotelDetailsBinding.inflate(layoutInflater)
     }
+
+    private val hotelId: Long by lazy { intent.getLongExtra(EXTRA_HOTEL_ID, -1) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        if(savedInstanceState == null){
+            showHotelDetaisFragment()
+        }
     }
+
+    private fun showHotelDetaisFragment() {
+        val fragment = HotelDetailsFragment.newInstance(hotelId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.details, fragment, HotelDetailsFragment.TAG_DETAILS)
+            .commit()
+    }
+
+
 }
