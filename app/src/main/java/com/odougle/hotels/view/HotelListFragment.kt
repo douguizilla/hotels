29e482fd0e5e4ce65b72fb9fd96ad725627881d0
douguizilla <douglasgommez@gmail.com>
2021-08-18 13:02:20 -0300

    implemented showHotelDetails to listen activity clicks and get the position clicked to call showHotelDetails from presenter passing a hotel object

--- app/src/main/java/com/odougle/hotels/view/HotelListFragment.kt
+++ app/src/main/java/com/odougle/hotels/view/HotelListFragment.kt
@@ -1,7 +1,9 @@
 package com.odougle.hotels.view
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.ListFragment
 import com.odougle.hotels.model.Hotel
 import com.odougle.hotels.model.MemoryRepository
@@ -22,6 +24,19 @@ class HotelListFragment : ListFragment(), HotelListView {
     }
 
     override fun showHotelDetails(hotel: Hotel) {
-        //implementarei em breve
+       if(activity is OnHotelClickListener){
+           val listener = activity as OnHotelClickListener
+           listener.onHotelClick(hotel)
+       }
+    }
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+        super.onListItemClick(l, v, position, id)
+        val hotel = l?.getItemAtPosition(position) as Hotel
+        presenter.showHotelDetails(hotel)
+    }
+
+    interface OnHotelClickListener{
+        fun onHotelClick(hotel: Hotel)
     }
 }
