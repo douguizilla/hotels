9792f4c9cf681b9607314d6784ea4225822f726c
douguizilla <douglasgommez@gmail.com>
2021-08-21 11:24:56 -0300

    implemented HotelFormFragment.kt

--- app/src/main/java/com/odougle/hotels/view/HotelFormFragment.kt
+++ app/src/main/java/com/odougle/hotels/view/HotelFormFragment.kt
@@ -1,21 +1,110 @@
 package com.odougle.hotels.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import com.odougle.hotels.R
+import com.odougle.hotels.databinding.FragmentHotelFormBinding
 import com.odougle.hotels.model.Hotel
+import com.odougle.hotels.model.MemoryRepository
+import com.odougle.hotels.presenter.HotelFormPresenter
 
 class HotelFormFragment : DialogFragment(), HotelFormView {
 
+    private val binding: FragmentHotelFormBinding by lazy {
+        FragmentHotelFormBinding.inflate(layoutInflater)
+    }
+    private val presenter = HotelFormPresenter(this, MemoryRepository)
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hotel_form, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val hotelId = arguments?.getLong(EXTRA_HOTEL_ID, 0) ?: 0
+        presenter.loadHotel(hotelId)
+        binding.edtAddress.setOnEditorActionListener { _, i, _ ->
+            handleKeyboardEvent(i)
+        }
+        dialog?.setTitle(R.string.action_new_hotel)
+        //abre o teclado virtual ao exibir o dialog
+        dialog?.window?.setSoftInputMode(
+            WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE
+        )
+    }
+
+    private fun handleKeyboardEvent(actionId: Int) : Boolean {
+        if(EditorInfo.IME_ACTION_DONE == actionId){
+            val hotel = saveHotel()
+            if(hotel != null){
+                if(activity is OnHotelSavedListener){
+                    val listener = activity as OnHotelSavedListener
+                    listener.onHotelSaved(hotel)
+                }
+                // fechar o dialog
+                dialog?.dismiss()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun saveHotel(): Hotel? {
+        val hotel = Hotel()
+        val hotelId = arguments?.getLong(EXTRA_HOTEL_ID, 0) ?: 0
+        hotel.id = hotelId
+        hotel.name = binding.edtName.text.toString()
+        hotel.address = binding.edtAddress.text.toString()
+        hotel.rating = binding.rtbRating.rating
+        if(presenter.saveHotel(hotel)){
+            return hotel
+        }
+            return null
+    }
 
     override fun showHotel(hotel: Hotel) {
-        TODO("Not yet implemented")
+        binding.edtName.setText(hotel.name)
+        binding.edtAddress.setText(hotel.address)
+        binding.rtbRating.rating = hotel.rating
     }
 
     override fun errorInvalidHotel() {
-        TODO("Not yet implemented")
+        Toast.makeText(requireContext(), R.string.error_invalid_hotel, Toast.LENGTH_SHORT).show()
     }
 
     override fun errorSaveHotel() {
-        TODO("Not yet implemented")
+        Toast.makeText(requireContext(), R.string.error_hotel_not_found, Toast.LENGTH_SHORT).show()
+    }
+
+    fun open(fm: FragmentManager){
+        if(fm.findFragmentByTag(DIALOG_TAG) == null){
+            show(fm,DIALOG_TAG)
+        }
+    }
+
+    interface OnHotelSavedListener{
+        fun onHotelSaved(hotel: Hotel)
+    }
+
+    companion object{
+        private const val EXTRA_HOTEL_ID = "hotel_id"
+        private const val DIALOG_TAG = "editDialog"
+
+        fun newInstance(hotelId: Long = 0) = HotelFormFragment().apply {
+            arguments = Bundle().apply {
+                putLong(EXTRA_HOTEL_ID, hotelId)
+            }
+        }
     }
 }
