754cf53b9d876571fe43c0b627fc3418e5e32d9c
douguizilla <douglasgommez@gmail.com>
2021-08-21 10:58:54 -0300

    created HotelFormFragment.kt

---
+++ app/src/main/java/com/odougle/hotels/view/HotelFormFragment.kt
@@ -0,0 +1,21 @@
+package com.odougle.hotels.view
+
+import androidx.fragment.app.DialogFragment
+import com.odougle.hotels.model.Hotel
+
+class HotelFormFragment : DialogFragment(), HotelFormView {
+
+
+
+    override fun showHotel(hotel: Hotel) {
+        TODO("Not yet implemented")
+    }
+
+    override fun errorInvalidHotel() {
+        TODO("Not yet implemented")
+    }
+
+    override fun errorSaveHotel() {
+        TODO("Not yet implemented")
+    }
+}
