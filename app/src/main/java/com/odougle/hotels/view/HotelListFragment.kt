778165cf42f07ae46f3758b8ae8a6fa1576c6835
douguizilla <douglasgommez@gmail.com>
2021-08-22 12:51:10 -0300

    set interfaces on HotelListFragment.kt and put its functions body to be implemented

--- app/src/main/java/com/odougle/hotels/view/HotelListFragment.kt
+++ app/src/main/java/com/odougle/hotels/view/HotelListFragment.kt
@@ -1,22 +1,29 @@
 package com.odougle.hotels.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.view.ActionMode
 import androidx.fragment.app.ListFragment
 import com.odougle.hotels.R
 import com.odougle.hotels.model.Hotel
 import com.odougle.hotels.model.MemoryRepository
 import com.odougle.hotels.presenter.HotelListPresenter
 
-class HotelListFragment : ListFragment(), HotelListView {
+class HotelListFragment : ListFragment(), HotelListView, ActionMode.Callback,
+    AdapterView.OnItemLongClickListener {
 
     private val presenter = HotelListPresenter(this, MemoryRepository)
+    private var actionMode: ActionMode? = null
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         presenter.searchHotels("")
+        listView.onItemLongClickListener = this
     }
 
     override fun showHotels(hotels: List<Hotel>) {
@@ -47,5 +54,46 @@ class HotelListFragment : ListFragment(), HotelListView {
 
     fun clearSearch(){
         presenter.searchHotels("")
+    }
+
+    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroyActionMode(mode: ActionMode?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showDeleteMode() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideDeleteMode() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showSelectedHotels(hotels: List<Hotel>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateSelectionCountText(count: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemLongClick(
+        parent: AdapterView<*>?,
+        view: View?,
+        position: Int,
+        id: Long
+    ): Boolean {
+        TODO("Not yet implemented")
     }
 }
