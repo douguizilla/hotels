d3e741baf0aff0f0b31e36036d1782a364362247
douguizilla <douglasgommez@gmail.com>
2021-08-22 12:26:08 -0300

    modified HotelListView interface putting some extra functions

--- app/src/main/java/com/odougle/hotels/view/HotelListView.kt
+++ app/src/main/java/com/odougle/hotels/view/HotelListView.kt
@@ -5,4 +5,8 @@ import com.odougle.hotels.model.Hotel
 interface HotelListView {
     fun showHotels(hotels: List<Hotel>)
     fun showHotelDetails(hotel: Hotel)
+    fun showDeleteMode()
+    fun hideDeleteMode()
+    fun showSelectedHotels(hotels: List<Hotel>)
+    fun updateSelectionCountText(count: Int)
 }
