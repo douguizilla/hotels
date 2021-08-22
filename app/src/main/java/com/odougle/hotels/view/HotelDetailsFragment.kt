d6b6aca1d35dda3d5714fe3d2cd134d1c9443fda
douguizilla <douglasgommez@gmail.com>
2021-08-22 17:09:06 -0300

    implemented onCreateOptionsMenu to add share action and setShareIntent to prepare share intent

--- app/src/main/java/com/odougle/hotels/view/HotelDetailsFragment.kt
+++ app/src/main/java/com/odougle/hotels/view/HotelDetailsFragment.kt
@@ -1,9 +1,10 @@
 package com.odougle.hotels.view
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import com.odougle.hotels.R
 import com.odougle.hotels.databinding.FragmentHotelDetailsBinding
@@ -19,14 +20,33 @@ class HotelDetailsFragment : Fragment(), HotelDetailsView {
     private val presenter = HotelDetailsPresenter(this, MemoryRepository)
     private var hotel: Hotel? = null
 
+    private var shareActionProvider : ShareActionProvider? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setHasOptionsMenu(true)
         _binding = FragmentHotelDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.hotel_details, menu)
+        val shareItem = menu?.findItem(R.id.action_share)
+        shareActionProvider = MenuItemCompat.getActionProvider(shareItem) as? ShareActionProvider
+        setShareIntent()
+    }
+
+    private fun setShareIntent() {
+        val text = getString(R.string.share_text, hotel?.name, hotel?.rating)
+        shareActionProvider?.setShareIntent(Intent(Intent.ACTION_SEND).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, text)
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
