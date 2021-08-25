aa5743478f75de3c18d8ee30c7b77ca77a7484cf
douguizilla <douglasgommez@gmail.com>
2021-08-25 15:17:13 -0300

    created SQLiteRepository.kt

---
+++ app/src/main/java/com/odougle/hotels/repository/sqlite/SQLiteRepository.kt
@@ -0,0 +1,8 @@
+package com.odougle.hotels.repository.sqlite
+
+import android.content.Context
+import com.odougle.hotels.repository.HotelRepository
+
+class SQLiteRepository(context: Context) : HotelRepository {
+
+}
