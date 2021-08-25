9d5bde24af0f10a0cbeaa619c88675475ea90a3d
douguizilla <douglasgommez@gmail.com>
2021-08-25 16:20:59 -0300

    created AndroidModule.kt with koin injections

---
+++ app/src/main/java/com/odougle/hotels/repository/di/AndroidModule.kt
@@ -0,0 +1,30 @@
+package com.odougle.hotels.repository.di
+
+import com.odougle.hotels.form.HotelFormPresenter
+import com.odougle.hotels.form.HotelFormView
+import com.odougle.hotels.list.HotelListPresenter
+import com.odougle.hotels.list.HotelListView
+import com.odougle.hotels.repository.HotelRepository
+import com.odougle.hotels.repository.sqlite.SQLiteRepository
+import org.koin.dsl.module.module
+
+val androidModule = module {
+    single { this }
+    single {
+        SQLiteRepository(context = get()) as HotelRepository
+    }
+
+    factory { (view: HotelListView) ->
+        HotelListPresenter(
+            view,
+            repository = get()
+        )
+    }
+
+    factory { (view: HotelFormView) ->
+        HotelFormPresenter(
+            view,
+            repository = get()
+        )
+    }
+}
