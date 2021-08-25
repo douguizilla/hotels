b29fec8a19d76f18cf348cce9ea0b602d4f8e35e
douguizilla <douglasgommez@gmail.com>
2021-08-25 15:16:59 -0300

    deleted TODOs

--- app/src/main/java/com/odougle/hotels/repository/sqlite/HotelSqlHelper.kt
+++ app/src/main/java/com/odougle/hotels/repository/sqlite/HotelSqlHelper.kt
@@ -1,9 +1,18 @@
 package com.odougle.hotels.repository.sqlite
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class HotelSqlHelper(context: Context)
     : SQLiteOpenHelper(context, DATABASE_NAME,null, DATABASE_VERSION){
+
+    override fun onCreate(db: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
 
 }
