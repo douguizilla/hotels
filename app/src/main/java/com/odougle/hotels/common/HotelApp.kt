e427cf55f3a9d2554a7d51219d09beb66dc68367
douguizilla <douglasgommez@gmail.com>
2021-08-25 16:22:29 -0300

    created HotelApp.kt to start the application

---
+++ app/src/main/java/com/odougle/hotels/common/HotelApp.kt
@@ -0,0 +1,18 @@
+package com.odougle.hotels.common
+
+import android.app.Application
+import com.odougle.hotels.repository.di.androidModule
+import org.koin.android.ext.android.startKoin
+import org.koin.standalone.StandAloneContext.stopKoin
+
+class HotelApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, listOf(androidModule))
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        stopKoin()
+    }
+}
