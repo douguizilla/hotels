495a5cb5c3f683fe44994e3ab639fc49c7593db1
douguizilla <douglasgommez@gmail.com>
2021-08-27 19:04:23 -0300

    refactored Hotel.kt to use persistence by Room database

--- app/src/main/java/com/odougle/hotels/model/Hotel.kt
+++ app/src/main/java/com/odougle/hotels/model/Hotel.kt
@@ -1,6 +1,15 @@
 package com.odougle.hotels.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.odougle.hotels.repository.sqlite.COLUMN_ID
+import com.odougle.hotels.repository.sqlite.TABLE_HOTEL
+
+@Entity(tableName = TABLE_HOTEL)
 data class Hotel(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = COLUMN_ID)
     var id: Long = 0,
     var name: String = "",
     var address: String = "",
