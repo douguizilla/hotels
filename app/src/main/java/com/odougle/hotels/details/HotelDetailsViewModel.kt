e38ee52aca11ef082cb98175764412e4039fb92f
douguizilla <douglasgommez@gmail.com>
2021-08-28 18:37:22 -0300

    implemented loadHotelDetails

--- app/src/main/java/com/odougle/hotels/details/HotelDetailsViewModel.kt
+++ app/src/main/java/com/odougle/hotels/details/HotelDetailsViewModel.kt
@@ -1,9 +1,15 @@
 package com.odougle.hotels.details
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.odougle.hotels.model.Hotel
 import com.odougle.hotels.repository.HotelRepository
 
 class HotelDetailsViewModel(
     private val repository: HotelRepository
 ) : ViewModel(){
+
+    fun loadHotelDetails(id: Long): LiveData<Hotel>{
+        return repository.hotelById(id)
+    }
 }
