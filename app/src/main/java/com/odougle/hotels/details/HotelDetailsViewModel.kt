c9500f2b6b7803802aad263786c1605cdc3efee3
douguizilla <douglasgommez@gmail.com>
2021-08-28 18:35:21 -0300

    created HotelDetailsViewModel.kt

---
+++ app/src/main/java/com/odougle/hotels/details/HotelDetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.odougle.hotels.details
+
+import androidx.lifecycle.ViewModel
+import com.odougle.hotels.repository.HotelRepository
+
+class HotelDetailsViewModel(
+    private val repository: HotelRepository
+) : ViewModel(){
+}
