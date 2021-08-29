f37daf38bb9d33326256429e1252cfd212d4ca85
douguizilla <douglasgommez@gmail.com>
2021-08-28 21:13:15 -0300

    deleted unused classes

--- app/src/main/java/com/odougle/hotels/form/HotelFormView.kt
+++
@@ -1,9 +0,0 @@
-package com.odougle.hotels.form
-
-import com.odougle.hotels.model.Hotel
-
-interface HotelFormView {
-    fun showHotel(hotel: Hotel)
-    fun errorInvalidHotel()
-    fun errorSaveHotel()
-}
